b0c08b472420bbef8acf72fd372ce20993eccfcc
zainwaniofficial <zainwani553@gmail.com>
2025-07-03 10:24:07 +0530

    make some questions using string

--- Strings/Questions/PalendromeCheck.js
+++ Strings/Questions/PalendromeCheck.js
@@ -10,5 +10,31 @@ const palendromeCheck = (string)=>{
 
 }
 
-palendromeCheck("hahah")
+// palendromeCheck("hahah")
+
+//another way to make pallindrome check without less space is 
+
+
+let str = "zaza";
+
+let isPallendrome = true;
+
+let i = 0;
+let j = str.length-1;
+
+while(i<j){
+    if(str.charAt(i) != str.charAt(j)){
+        isPallendrome = false;
+    }
+    i++;
+    j--;
+}
+
+if(isPallendrome){
+    console.log("it is pallendrome");
+    
+}else{
+    console.log("it is not pallendrome");
+    
+}
 
